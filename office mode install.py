e5fe3164cf0d7d1d14a5c325966607ef081c245c
nexusdev999 <newnexuxdev@gmail.com>
2026-01-27 20:28:09 -0300

    Create office mode install.py
    apenas comecei (não esta finalizado e nem em ponto de uso, alerta de perigo)

---
+++ office mode install.py
@@ -0,0 +1,10 @@
+#imports
+import os, subprocess, time, platform
+
+#variaveis de sistema
+limpar = os.system("clear")
+arqutetura = platform.machine()
+sistema = platform.system()
+
+#lista de apps
+apps_office = []
