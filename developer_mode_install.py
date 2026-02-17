b763d998c3c6dc2f445debad89df6a39fd5583bb
nexuxdev <newnexuxdev@gmail.com>
2026-02-17 11:38:41 -0300

    mudei completamene o codigo adicionando simplesmente escolhas

--- developer_mode_install.py
+++ developer_mode_install.py
@@ -1,43 +1,52 @@
 # import
-import subprocess
+import subprocess, os
 
+clear = os.system("clear")
 
 print("""
 
+--------------------------------
 
-    PosNext Developer Mode Installer
-    Installing Developer Mode Apps...
-    
+LISTA DE APPS PARA DOWNLOAD V1
+-cachyos
+
+---------------------------------
+
+            [1]linux toys
+
+[2]base e depedencias   | [7]Ghidra
+
+[3]github desktop       | [8]dev toolbox
+
+[4]visual studio code   | [9]qt creator
+|open souce
+
+[5]kitty                | [10]zed
+
+[6]geany                | [11]postman
+
+[12]unity hub           | [13]gamemaker2
+
+[14]Meld                | [15]docker
+ 
+[16]Typora/Ghostwriter  | [17]Typora/Ghostwriter
+
+[18]oh my bash          | [19]Warp
 
 
-    """)
-# lista de apps
-apps_dev = [
-    "git",
-    "visual-studio-code-bin",
-    "zed",
-    "github-desktop",
-    "vim",
-    "neovim",
-    "kitty",
-    "jetbrains-toolbox",
-    "mise",
-    "docker",
-    "lazydocker",
-    "lazygit",
-]
+""")
+
+select = int(input("Digite o numero da opção escolhida: "))
+print("boa escolha para seu workspace")
+
+if select == 1:
+    os.system("clear")
+    os.system("curl -fsSL https://linux.toys/install.sh | bash")
+else:
+    os.system("clear")
+    print("não temos essa opção")
+    os.system("python developer_mode_install.py")
+    os.system("para sair e só fechar o terminal")
 
 
-def instalar(app):
-    subprocess.run(["sudo", "pacman", "-S", "--noconfirm", app])
-   
-   
-   
-   
-    
-for app in apps_dev:
-    instalar(app)
 
-    
-
-    print("Developer Mode Apps Installed Successfully!")
